b06a473f18d6d6f6c1fda2a40ba38e9a18bca47d
Palash Pandey <pxp142@jefferson.edu>
2020-01-21 17:27:47 -0500

    deleted csv parsing

--- CourseRecommendation/server.py
+++ CourseRecommendation/server.py
@@ -10,13 +10,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
 
-def parse_get_csv(csv_url):
-	csv_raw = requests.get("https://raw.githubusercontent.com/PalashPandey/TextProcessingWorkingGroup/master/CourseRecommendation/static/course_df.csv").text
-	data_df = pd.DataFrame(csv_raw)
-	return data_df
 
 
-@app.route('/homepage')
+@app.route('/')
 def homepage():
 	return render_template('old_index.html' )
 
