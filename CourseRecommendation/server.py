2729b1cb71c6b05f1076c95ba7b3da87bf746eeb
Palash Pandey <pxp142@jefferson.edu>
2020-01-21 17:12:48 -0500

    parse csv from get to dataframe

--- CourseRecommendation/server.py
+++ CourseRecommendation/server.py
@@ -4,13 +4,16 @@ import pandas as pd
 import math
 import json
 from pandas.io.json import json_normalize
+import requests
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
 
-
-
+def parse_get_csv(csv_url):
+	csv_raw = requests.get("https://raw.githubusercontent.com/PalashPandey/TextProcessingWorkingGroup/master/CourseRecommendation/static/course_df.csv").text
+	data_df = pd.DataFrame(csv_raw)
+	return data_df
 
 
 @app.route('/homepage')
